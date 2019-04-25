606a2d90ac0a01d806142f6a3450552f137f6ae1
choijunyong <skdd456@naver.com>
2019-04-25 09:53:35 +0900

    modify

--- 7576/Main.java
+++ 7576/Main.java
@@ -1,107 +1,113 @@
+package exam1;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+public class main 
+{
+	static int[][] map;
+	static Queue<String> q = new LinkedList<>();
+	static Queue<Integer> q1 = new LinkedList<>();
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
+	//	static int[][] map;
+	static int[] edgemap;
+	static boolean[][] visit;
+	static int ver, edge, cnt, row, col,NX,NY;
+	static List<Integer> l = new LinkedList<>();
+	static int result = 0;
 
-    static int c,r;
-    static Stack<Integer> s = new Stack<Integer>();
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static int line[][] = new int[1000][1000];
-    static int nextrow=0;
-    static int nextcol=0;
-    static int[][]map;
-    static boolean visit[][];
-    static Queue<Integer> q=  new LinkedList<Integer>();
-    static int row , col;
+	static void bfs () {
 
-    static void BFS()
-    {
-        while (!q.isEmpty())
-        {
-            row = q.poll();
-            col = q.poll();
-            for (int i = 0; i <4 ; i++)
-            {
-                nextrow = row + dx[i];
-                nextcol = col + dy[i];
-                visit[row][col] =true;
-                if ((nextcol < c && nextcol >=0) && (nextrow >=0 && nextrow < r))
-                {
-                    if (map[nextrow][nextcol] == 0  && !visit[nextrow][nextcol])
-                    {
-                        map[nextrow][nextcol] =1;
-                        visit[nextrow][nextcol]=true;
-                        line[nextrow][nextcol] = line[row][col]+1;
-                        if (line[nextrow][nextcol] != 0) {
-                            s.push(line[nextrow][nextcol]);
-                        }
-                        q.add(nextrow);
-                        q.add(nextcol);
-                    }
-                }
-            }
-        }
-// 행렬 확인
-//        for (int i = 1; i <=n ; i++) {
-//            for (int j = 1; j <=m ; j++) {
-//                System.out.print(map[i][j]);
-//            }
-//            System.out.println();
-//
-//        }
+		while(!q1.isEmpty())
+		{
+			int CX = q1.poll();
+			int CY = q1.poll();
+			visit[CX][CY] = true;
+			for (int i = 0; i < dx.length; i++) 
+			{
+				NX = CX +dx[i];
+				NY = CY +dy[i];
+				if (NX < 1 || NX > row || NY < 1 || NY > col) {
+					continue;
+				}
+				if (map[NX][NY]==0 && !visit[NX][NY]) {
 
-    }
-    public static void main(String[] args) throws IOException {
+					q1.add(NX);
+					q1.add(NY);
+					visit[NX][NY]= true;
+					map[NX][NY] = map[CX][CY]+1;
+				}
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException 
+	{
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(bf.readLine());
-        c = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        map = new int[r][c];
-        visit = new boolean[r][c];
-        for (int i = 0; i < r; i++)
-        {
-            st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < c ; j++)
-            {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (map[i][j] == 1) {
 
-                    q.add(i);
-                    q.add(j);
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st ;
+		st = new StringTokenizer(bf.readLine());
+		col = Integer.parseInt(st.nextToken());
+		row = Integer.parseInt(st.nextToken());
 
-                }
-            }
-        }
-        BFS();
-        for (int i = 0; i < r; i++)
-        {
+		map= new int [row+1][col+1];
+		visit  = new boolean[row+1][col+1];
+		for (int i = 1; i <= row; i++) {
+			st = new StringTokenizer(bf.readLine());
 
-            for (int j = 0; j < c ; j++)
-            {
-                if (map[i][j] == 0) {
-                    System.out.println("-1");
-                    return;
-                }
+			for (int j = 1; j <= col; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-            }
-        }
-        if (s.isEmpty()) {
-            System.out.println(line[nextrow][nextcol]);
-        }
-        else {
-            System.out.println(s.pop());
-        }
-    }
+		for (int i = 1; i <= row; i++) {
+			for (int j = 1; j <= col; j++) {
+				if (map[i][j]==1) {
+					q1.add(i);
+					q1.add(j);
+				}
+
+			}
+		}
+		bfs();
+
+		int max = 0;
+		for (int i = 1; i <= row; i++) {
+			for (int j = 1; j <= col; j++) {
+				if (map[i][j]==0) {
+					System.out.println("-1");
+					return ;
+				}
+				if (map[i][j]>max) {
+					max  = map[i][j];
+				}
+			}
+
+
+
+			
+
+		}
+		System.out.println(max-1);
+	}
 }
+
+
+class pair {
+	int x, y;
+
+	pair(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+
+}
+
